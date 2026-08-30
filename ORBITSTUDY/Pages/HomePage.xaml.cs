df28864a1ec4dc8bc5faebae54049d5f2b9da7a6
GS1-HUB <2241539@my.ipleiria.pt>
2026-08-30 14:58:15 +0100

    added a namiation floating for center icon on homepage

--- ORBITSTUDY/Pages/HomePage.xaml.cs
+++ ORBITSTUDY/Pages/HomePage.xaml.cs
@@ -7,8 +7,30 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
-			
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        imgCenter.Opacity = 0;
+        imgCenter.TranslationY = 30;
+
+        await Task.WhenAll(
+            imgCenter.FadeToAsync(1, 1000, Easing.CubicOut),
+            imgCenter.TranslateToAsync(0, 0, 1000, Easing.SinInOut));
+
+        _ = StarFloatingAnimation();
+    }
+
+    private async Task StarFloatingAnimation()
+    {
+        while (true)
+        {
+            await imgCenter.TranslateToAsync(0, -12, 2000, Easing.SinInOut);
+            await imgCenter.TranslateToAsync(0, 12, 2000, Easing.SinInOut);
+        }
+    }
 
     private async void btnSettings_Clicked(object sender, EventArgs e)
     {
